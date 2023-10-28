2c6ab0610ce7dbe2d383baec2dd4fb77784d9d47
shangxuwei <2736274271@qq.com>
2023-10-28 17:33:36 +0800

    second commit

---
+++ login.py
@@ -0,0 +1,35 @@
+from tkinter import *
+
+class MY_GUI():
+    def __init__(self,init_window_name):
+        self.init_window_name = init_window_name
+
+    def set_init_window(self):
+        self.init_window_name.title("聊天软件")  # 窗口名
+        self.init_window_name.geometry('800x600+10+10')
+        self.init_data_label = Label(self.init_window_name, text="聊天记录")
+        self.init_data_label.grid(row=0, column=0)
+        self.result_data_label = Label(self.init_window_name, text="输入")
+        self.result_data_label.grid(row=20, column=0)
+
+        self.init_data_Text = Text(self.init_window_name, width=110, height=30)
+        self.init_data_Text.grid(row=1, column=0, rowspan=10, columnspan=10)
+        self.log_data_Text = Text(self.init_window_name, width=110, height=4)
+        self.log_data_Text.grid(row=21, column=0, columnspan=10)
+
+        self.send_button = Button(self.init_window_name, text="send", bg="lightblue", width=10,)
+        self.send_button.grid(row=23, column=8)
+        self.send_button = Button(self.init_window_name, text="表情包", width=10, )
+        self.send_button.grid(row=20, column=4)
+        self.send_button = Button(self.init_window_name, text="文件", width=10, )
+        self.send_button.grid(row=20, column=8)
+
+# def gui_start():
+#     init_window = Tk()
+#     ZMJ_PORTAL = MY_GUI(init_window)
+#     ZMJ_PORTAL.set_init_window()
+#
+#     init_window.mainloop()
+#
+#
+# gui_start()
--- main.py
+++ main.py
@@ -1,53 +1,134 @@
-from tkinter import *
+import tkinter as tk  # 使用Tkinter前需要先导入
+import tkinter.messagebox
+import pickle
+import login
+# 第1步，实例化object，建立窗口window
+window = tk.Tk()
 
-class MY_GUI():
-    def __init__(self,init_window_name):
-        self.init_window_name = init_window_name
+# 第2步，给窗口的可视化起名字
+window.title('wechat')
 
-    def set_init_window(self):
-        self.init_window_name.title("聊天软件")  # 窗口名
-        self.init_window_name.geometry('420x100+10+10')
-        self.init_data_label = Label(self.init_window_name, text="账户")
-        self.init_data_label.grid(row=0, column=0)
-        self.result_data_label = Label(self.init_window_name, text="密码")
-        self.result_data_label.grid(row=20, column=0)
+# 第3步，设定窗口的大小(长 * 宽)
+window.geometry('400x300')  # 这里的乘是小x
 
-        self.init_data_Text = Text(self.init_window_name, width=50, height=2)
-        self.init_data_Text.grid(row=0, column=2, rowspan=10, columnspan=10)
-        self.log_data_Text = Text(self.init_window_name, width=50, height=2)
-        self.log_data_Text.grid(row=20, column=2, columnspan=10)
+# 第4步，加载 wellcome image
+# canvas = tk.Canvas(window, width=400, height=135, bg='green')
+# image_file = tk.PhotoImage(file='pic.gif')
+# image = canvas.create_image(200, 0, anchor='n', image=image_file)
+# canvas.pack(side='top')
+# tk.Label(window, text='Wellcome', font=('Arial', 16)).pack()
 
-        self.send_button = Button(self.init_window_name, text="登录", width=10,command=self.set_init_window2)
-        self.send_button.grid(row=30, column=2)
-        self.send_button = Button(self.init_window_name, text="注册", width=10, )
-        self.send_button.grid(row=30, column=6)
+# 第5步，用户信息
+tk.Label(window, text='User name:', font=('Arial', 14)).place(x=10, y=50)
+tk.Label(window, text='Password:', font=('Arial', 14)).place(x=10, y=90)
 
-    def set_init_window2(self):
-        self.init_window_name.title("聊天软件")
-        self.init_window_name.geometry('1068x681+10+10')
-        self.init_data_label = Label(self.init_window_name, text="聊天记录")
-        self.init_data_label.grid(row=0, column=0)
-        self.result_data_label = Label(self.init_window_name, text="输入")
-        self.result_data_label.grid(row=12, column=0)
+# 第6步，用户登录输入框entry
+# 用户名
+var_usr_name = tk.StringVar()
+var_usr_name.set('example@python.com')
+entry_usr_name = tk.Entry(window, textvariable=var_usr_name, font=('Arial', 14))
+entry_usr_name.place(x=120, y=55)
+# 用户密码
+var_usr_pwd = tk.StringVar()
+entry_usr_pwd = tk.Entry(window, textvariable=var_usr_pwd, font=('Arial', 14), show='*')
+entry_usr_pwd.place(x=120, y=95)
 
-        self.init_data_Text = Text(self.init_window_name, width=150, height=35)
-        self.init_data_Text.grid(row=1, column=0, rowspan=10, columnspan=10)
-        self.log_data_Text = Text(self.init_window_name, width=150, height=5)
-        self.log_data_Text.grid(row=13, column=0, columnspan=10)
+# 第8步，定义用户登录功能
+def usr_login():
+    # 这两行代码就是获取用户输入的usr_name和usr_pwd
+    usr_name = var_usr_name.get()
+    usr_pwd = var_usr_pwd.get()
 
-        self.send_button = Button(self.init_window_name, text="send", bg="lightblue", width=10,)
-        self.send_button.grid(row=18, column=4)
-        self.send_button = Button(self.init_window_name, text="表情包", width=10, )
-        self.send_button.grid(row=12, column=4)
-        self.send_button = Button(self.init_window_name, text="文件", width=10, )
-        self.send_button.grid(row=12, column=8)
+    # 这里设置异常捕获，当我们第一次访问用户信息文件时是不存在的，所以这里设置异常捕获。
+    # 中间的两行就是我们的匹配，即程序将输入的信息和文件中的信息匹配。
+    try:
+        with open('usrs_info.pickle', 'rb') as usr_file:
+            usrs_info = pickle.load(usr_file)
+    except FileNotFoundError:
+        # 这里就是我们在没有读取到`usr_file`的时候，程序会创建一个`usr_file`这个文件，并将管理员
+        # 的用户和密码写入，即用户名为`admin`密码为`admin`。
+        with open('usrs_info.pickle', 'wb') as usr_file:
+            usrs_info = {'admin': 'admin'}
+            pickle.dump(usrs_info, usr_file)
+            usr_file.close()  # 必须先关闭，否则pickle.load()会出现EOFError: Ran out of input
 
-def gui_start():
-    init_window = Tk()
-    ZMJ_PORTAL = MY_GUI(init_window)
-    ZMJ_PORTAL.set_init_window()
+    # 如果用户名和密码与文件中的匹配成功，则会登录成功，并跳出弹窗how are you? 加上你的用户名。
+    if usr_name in usrs_info:
+        if usr_pwd == usrs_info[usr_name]:
+            tkinter.messagebox.showinfo(title='Welcome', message='How are you? ' + usr_name)
+            init_window = tk.Tk()
+            ZMJ_PORTAL = login.MY_GUI(init_window)
+            ZMJ_PORTAL.set_init_window()
+            init_window.mainloop()
+        # 如果用户名匹配成功，而密码输入错误，则会弹出'Error, your password is wrong, try again.'
+        else:
+            tkinter.messagebox.showerror(message='Error, your password is wrong, try again.')
+    else:  # 如果发现用户名不存在
+        is_sign_up = tkinter.messagebox.askyesno('Welcome！ ', 'You have not sign up yet. Sign up now?')
+        # 提示需不需要注册新用户
+        if is_sign_up:
+            usr_sign_up()
 
-    init_window.mainloop()
+
+# 第9步，定义用户注册功能
+def usr_sign_up():
+    def sign_to_wechat():
+        # 以下三行就是获取我们注册时所输入的信息
+        np = new_pwd.get()
+        npf = new_pwd_confirm.get()
+        nn = new_name.get()
+
+        # 这里是打开我们记录数据的文件，将注册信息读出
+        with open('usrs_info.pickle', 'rb') as usr_file:
+            exist_usr_info = pickle.load(usr_file)
+        # 这里就是判断，如果两次密码输入不一致，则提示Error, Password and confirm password must be the same!
+        if np != npf:
+            tkinter.messagebox.showerror('Error', 'Password and confirm password must be the same!')
+
+        # 如果用户名已经在我们的数据文件中，则提示Error, The user has already signed up!
+        elif nn in exist_usr_info:
+            tkinter.messagebox.showerror('Error', 'The user has already signed up!')
+
+        # 最后如果输入无以上错误，则将注册输入的信息记录到文件当中，并提示注册成功Welcome！,You have successfully signed up!，然后销毁窗口。
+        else:
+            exist_usr_info[nn] = np
+            with open('usrs_info.pickle', 'wb') as usr_file:
+                pickle.dump(exist_usr_info, usr_file)
+            tkinter.messagebox.showinfo('Welcome', 'You have successfully signed up!')
+            # 然后销毁窗口。
+            window_sign_up.destroy()
+
+    # 定义长在窗口上的窗口
+    window_sign_up = tk.Toplevel(window)
+    window_sign_up.geometry('300x200')
+    window_sign_up.title('Sign up window')
+
+    new_name = tk.StringVar()  # 将输入的注册名赋值给变量
+    new_name.set('example@python.com')  # 将最初显示定为'example@python.com'
+    tk.Label(window_sign_up, text='User name: ').place(x=10, y=10)  # 将`User name:`放置在坐标（10,10）。
+    entry_new_name = tk.Entry(window_sign_up, textvariable=new_name)  # 创建一个注册名的`entry`，变量为`new_name`
+    entry_new_name.place(x=130, y=10)  # `entry`放置在坐标（150,10）.
+
+    new_pwd = tk.StringVar()
+    tk.Label(window_sign_up, text='Password: ').place(x=10, y=50)
+    entry_usr_pwd = tk.Entry(window_sign_up, textvariable=new_pwd, show='*')
+    entry_usr_pwd.place(x=130, y=50)
+
+    new_pwd_confirm = tk.StringVar()
+    tk.Label(window_sign_up, text='Confirm password: ').place(x=10, y=90)
+    entry_usr_pwd_confirm = tk.Entry(window_sign_up, textvariable=new_pwd_confirm, show='*')
+    entry_usr_pwd_confirm.place(x=130, y=90)
+
+    # 下面的 sign_to_wechat
+    btn_comfirm_sign_up = tk.Button(window_sign_up, text='Sign up', command=sign_to_wechat)
+    btn_comfirm_sign_up.place(x=180, y=120)
 
 
-gui_start()
+# 第7步，login and sign up 按钮
+btn_login = tk.Button(window, text='Login', command=usr_login)
+btn_login.place(x=100, y=200)
+btn_sign_up = tk.Button(window, text='Sign up', command=usr_sign_up)
+btn_sign_up.place(x=200, y=200)
+
+# 第10步，主窗口循环显示
+window.mainloop()
