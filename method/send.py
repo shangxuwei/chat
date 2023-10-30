36e260588dd5f13eb97d743db159b756fb85670b
mengyonghao <1533512157@qq.com>
2023-10-30 21:15:33 +0800

    服务器通信回显

--- method/send.py
+++ method/send.py
@@ -1,10 +1,16 @@
 import socket
+import time
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+hostname = socket.gethostname()
+ip = socket.gethostbyname(hostname)
+t = time.localtime()
+date = f'{t.tm_year},{t.tm_mon},{t.tm_mday},{t.tm_hour}:{t.tm_min}:{t.tm_sec}'
+name = 'shangxuwei'
 try:
-    msg = "chat\n\n 你好".encode('utf-8')
-    sent = sock.sendto(msg, ('10.106.187.124', 10088))
+    msg = f"CHAT\n\n{date}\n\n{name}\n\n你好".encode('utf-8')
+    sent = sock.sendto(msg, (ip, 10088))
     data, server = sock.recvfrom(4096)
-    print(data.decode())
+    print(data.decode("utf-8"))
 finally:
     sock.close()
--- method/serve.py
+++ method/serve.py
@@ -4,17 +4,37 @@ class Service:
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind(('0.0.0.0', 10088))
-
+        self.ip_cache = []
 
     def chat_listen(self):
         while True:
             data, address = self.sock.recvfrom(4096)
-            header, message = data.decode('utf-8').split("\n\n",1)
-            print(header,message)
+            if address not in self.ip_cache:
+                self.ip_cache.append(address)
+            header, date, name, message = data.decode('utf-8').split("\n\n",3)
+            method = {
+                'LOGIN': self.login,
+                'REGISTER': self.register,
+                'CHAT': self.chat,
+                'UPLOAD': self.upload,
+                'DOWNLOAD':self.download
+            }
+            method[header](date,name,message)
             if data:
-                self.sock.sendto('已接收到你发来的消息'.encode('UTF-8'), address)
-
-    def message(self):
+                # self.sock.sendto(f'{date}  {name}:{message}'.encode('UTF-8'), address)
+                pass
+    def login(self):
+        pass
+    def register(self):
+        pass
+    def chat(self, date, name, message):
+        print(f'{date}  {name}:{message}')
+        for address in self.ip_cache:
+            self.sock.sendto(f'{date}  {name}:{message}'.encode('UTF-8'), address)
+        pass
+    def upload(self):
+        pass
+    def download(self):
         pass
 
 if __name__ == "__main__":
