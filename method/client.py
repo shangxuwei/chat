5a425eff32e514d41e2cbb7cec5fa76c1f7388a8
mengyonghao <1533512157@qq.com>
2023-10-31 11:04:10 +0800

    新建服务端客户端通信

--- method/client.py
+++ method/client.py
@@ -1,16 +1,62 @@
 import socket
 import time
+from threading import Thread
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-hostname = socket.gethostname()
-ip = socket.gethostbyname(hostname)
-t = time.localtime()
-date = f'{t.tm_year},{t.tm_mon},{t.tm_mday},{t.tm_hour}:{t.tm_min}:{t.tm_sec}'
-name = 'shangxuwei'
-try:
-    msg = f"CHAT\n\n{date}\n\n{name}\n\n你好".encode('utf-8')
-    sent = sock.sendto(msg, (ip, 10088))
-    data, server = sock.recvfrom(4096)
-    print(data.decode("utf-8"))
-finally:
-    sock.close()
+class Client:
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # 本地端地址
+        self.sock.bind(('0.0.0.0',10087))
+        # 服务端地址
+        self.service = ('127.0.0.1', 10088)
+
+    def listen(self):
+        while True:
+            data, address = self.sock.recvfrom(4096)
+            method = {
+                'MESSAGE': self.message,
+                'LOGIN': self.login,
+                'REGISTER': self.register,
+                'CHAT': self.chat,
+                'UPLOAD': self.upload,
+                'DOWNLOAD': self.download,
+                'ERROR': self.error
+            }
+            header, message = data.decode('utf-8').split("\n\n",3)
+            method[header](message)
+
+
+
+    def message(self):
+        pass
+
+    def login(self):
+        pass
+
+    def register(self):
+        pass
+
+    def chat(self):
+        t = time.localtime()
+        date = f'{t.tm_year},{t.tm_mon},{t.tm_mday},{t.tm_hour}:{t.tm_min}:{t.tm_sec}'
+        name = 'shangxuwei'
+        msg = f"{date}\n\n{name}\n\n你好".encode('utf-8')
+        sent = self.sock.sendto(msg, self.service)
+        print(sent)
+        data, server = self.sock.recvfrom(4096)
+        print(data.decode("utf-8"))
+
+    def upload(self):
+        pass
+
+    def download(self):
+        pass
+
+    def error(self):
+        pass
+
+if __name__ == '__main__':
+    client = Client()
+    listen = Thread(target=client.listen)
+    listen.start()
+    client.chat()
--- method/serve.py
+++ method/serve.py
@@ -6,37 +6,47 @@ class Service:
         self.sock.bind(('0.0.0.0', 10088))
         self.ip_cache = []
 
-    def chat_listen(self):
+    def listen(self):
         while True:
+            print('1')
             data, address = self.sock.recvfrom(4096)
+            print('2')
             if address not in self.ip_cache:
                 self.ip_cache.append(address)
-            header, date, name, message = data.decode('utf-8').split("\n\n",3)
-            method = {
-                'LOGIN': self.login,
-                'REGISTER': self.register,
-                'CHAT': self.chat,
-                'UPLOAD': self.upload,
-                'DOWNLOAD':self.download
-            }
-            method[header](date,name,message)
-            if data:
-                # self.sock.sendto(f'{date}  {name}:{message}'.encode('UTF-8'), address)
-                pass
+            try:
+                header, date, name, message = data.decode('utf-8').split("\n\n",3)
+                method = {
+                    'LOGIN': self.login,
+                    'REGISTER': self.register,
+                    'CHAT': self.chat,
+                    'UPLOAD': self.upload,
+                    'DOWNLOAD':self.download
+                }
+                method[header](date,name,message)
+            except:
+                print('ERROR')
+                self.sock.sendto('ERROR\n\n'.encode('utf-8'),address)
+
     def login(self):
         pass
+
     def register(self):
         pass
+
     def chat(self, date, name, message):
         print(f'{date}  {name}:{message}')
         for address in self.ip_cache:
             self.sock.sendto(f'{date}  {name}:{message}'.encode('UTF-8'), address)
+
+        # TODO: SQL操作
         pass
+
     def upload(self):
         pass
+
     def download(self):
         pass
 
 if __name__ == "__main__":
     service = Service()
-    service.chat_listen()
+    service.listen()
