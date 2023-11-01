5508ac9ea638d3b667007dbf1f3641212df14408
mengyonghao <1533512157@qq.com>
2023-11-01 12:51:30 +0800

    关联client与login页面数据，实现按钮交互，修复ack回传，增加上线自动连接服务器，定时发送报文保持在线

--- method/client.py
+++ method/client.py
@@ -1,6 +1,7 @@
 import socket
 import time
 from threading import Thread
+import hashlib
 
 class Client:
     def __init__(self):
@@ -8,63 +9,83 @@ class Client:
         self.sock.bind(('0.0.0.0', 0))
         # 服务端地址
         self.service = ('127.0.0.1', 10088)
-        self.user = '123'
+        self.user = 'none'
 
-        self.rcv = []
+        self.ackpool = []
 
         listen = Thread(target=self.listen)
         listen.start()
+        keep = Thread(target=self.online)
+        keep.start()
 
     def listen(self):
         while True:
             data, address = self.sock.recvfrom(4096)
+            header,date,user,payload = data.decode('utf-8').split('\n\n',3)
             method = {
-                'MESSAGE': self.message,
-                'LOGIN': self.login,
-                'REGISTER': self.register,
+                'MESSAGE': [self.message,date,user,payload],
+                'LOGIN_BACK': self.login_back,
+                'REGISTER_BACK': self.register_back,
                 'UPLOAD': self.upload,
                 'DOWNLOAD': self.download,
                 'ERROR': self.error,
-                'ACK': self.ack
+                'ACK': [self.ack,date,user,payload]
             }
-            pkt = data.decode('utf-8').split("\n\n",3)
-            method[pkt[0]](pkt)
+            method[header][0](*(method[header][1:]))
 
     def online(self):
-        pass
-    def send(self,header,date,name,payload):
-        msg = f"{header}\n\n{date}\n\n{name}\n\n{payload}".encode('utf-8')
+        while True:
+            date = time.mktime(time.localtime())
+            self.send("ONLINE",date,self.user,'online')
+            time.sleep(60)
+
+    def send(self,header,date,user,payload):
+        msg = f"{header}\n\n{date}\n\n{user}\n\n{payload}".encode('utf-8')
         retry = 0
-        while not self.ack_check(hash(f'{header}{date}{name}')) and retry <= 5:
+        while not self.ack_check(hash(f'{header}{date}{user}')) and retry <= 5:
             self.sock.sendto(msg, self.service)
             time.sleep(1)
             retry += 1
         return retry <= 5
 
-    def ack(self,pkt):
-        header = pkt[1]
-        date = pkt[2]
-        name = pkt[3]
-        print(f'ack:{header}{date}{name}')
-        self.rcv.append(hash(f'{header}{date}{name}'))
+    def ack(self, header, date, user):
+        self.ackpool.append(hash(f'{header}{date}{user}'))
 
     def ack_check(self, flag):
-        if flag not in self.rcv:
+        if flag not in self.ackpool:
             return False
-        self.rcv.remove(flag)
+        self.ackpool.remove(flag)
         return True
 
-    def message(self,pkt):
-        header, date, name, payload = pkt
+    def message(self,date,user,payload):
         t = time.localtime(float(date))
         date = f'{t.tm_year},{t.tm_mon},{t.tm_mday},{t.tm_hour}:{t.tm_min}:{t.tm_sec}'
 
-        print(f'[{date}]{name}: {payload}')
+        print(f'[{date}]{user}: {payload}')
+        return f'[{date}]{user}: {payload}'
 
-    def login(self):
+    def login(self,user,password):
+        header = 'LOGIN'
+        date = time.mktime(time.localtime())
+        md5_object = hashlib.md5()
+        md5_object.update(password.encode('utf-8'))
+        md5_result = md5_object.hexdigest()
+        self.send(header,date,user,md5_result)
+        return 1
+
+    def login_back(self):
         pass
 
-    def register(self):
+    def register(self,user,password):
+        header = 'REGISTER'
+        date = time.mktime(time.localtime())
+        md5_object = hashlib.md5()
+        md5_object.update(password.encode('utf-8'))
+        md5_result = md5_object.hexdigest()
+        self.send(header, date, user, md5_result)
+        return 2
+
+    def register_back(self):
         pass
 
     def chat(self,message):
@@ -85,9 +106,9 @@ class Client:
     def error(self,pkt):
         print('error')
 
-if __name__ == '__main__':
-    client = Client()
-    while True:
-        msg = input('>>>')
-        print('\r'+' '*(len(msg)+3)+'\r')
-        client.chat(msg)
+# if __name__ == '__main__':
+#     client = Client()
+#     while True:
+#         msg = input('>>>')
+#         print('\r'+' '*(len(msg)+3)+'\r')
+#         client.chat(msg)
--- method/serve.py
+++ method/serve.py
@@ -5,48 +5,51 @@ class Service:
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind(('0.0.0.0', 10088))
-        self.ip_cache = []
+        self.ip_pool = []
 
     def listen(self):
         while True:
             data, address = self.sock.recvfrom(4096)
-            if data:
-                pass
-            if address not in self.ip_cache:
-                self.ip_cache.append(address)
             try:
-                header, date, name, payload = data.decode('utf-8').split("\n\n",3)
-                self.sock.sendto(f'ACK\n\n{header}\n\n{date}\n\n{name}'.encode('utf-8'),address)
+                header, date, user, payload = data.decode('utf-8').split("\n\n",3)
+                print(header,date,user,payload)
+                ack_mag = f'ACK\n\n{header}\n\n{date}\n\n{user}'.encode('utf-8')
+                self.sock.sendto(ack_mag, address)
                 method = {
-                    'LOGIN': self.login,
-                    'REGISTER': self.register,
-                    'MESSAGE': self.message,
+                    'LOGIN': [self.login,user,payload],
+                    'REGISTER': [self.register,user,payload],
+                    'MESSAGE': [self.message,date,user,payload],
                     'UPLOAD': self.upload,
                     'DOWNLOAD':self.download,
-                    'ONLINE':self.online
+                    'ONLINE':[self.online,address,user]
                 }
-                method[header](date,name,payload)
-                print(f'ACK to {address},{header}, {header}{date}{name}')
+
+                method[header][0](*(method[header][1:]))
+                ack_pak = f'{header}{date}{user}'
+                print(f'ACK to {address}, {header}, {user}, hash:{hash(ack_pak)}')
             except Exception as e:
                 print(e)
                 print(data.decode('utf-8'))
                 print('ERROR')
                 self.sock.sendto('ERROR\n\n \n\n \n\n '.encode('utf-8'),address)
 
-    def online(self):
-        pass
-    def login(self):
-        pass
+    def online(self,address,name):
+        if (name,address) not in self.ip_pool:
+            self.ip_pool.append((name,address))
 
-    def register(self):
+    def login(self,user,payload):
+        print('login:',user,payload)
+
+    def register(self,user,payload):
+        print('register:', user, payload)
         pass
 
     def message(self, date, name, payload):
         t = time.localtime(float(date))
         dt = f'{t.tm_year},{t.tm_mon},{t.tm_mday},{t.tm_hour}:{t.tm_min}:{t.tm_sec}'
-        print(f'[{dt}]{name}:{payload}')
-        for address in self.ip_cache:
-            self.sock.sendto(f'MESSAGE\n\n{date}\n\n{name}\n\n{payload}'.encode('UTF-8'), address)
+        for _,address in self.ip_pool:
+            msg = f'MESSAGE\n\n{date}\n\n{name}\n\n{payload}'.encode('UTF-8')
+            self.sock.sendto(msg, address)
 
         # TODO: SQL操作
         pass
