27a85ae625d8853a036f6a8886940e17a49ab9e3
mengyonghao <1533512157@qq.com>
2023-11-03 22:55:07 +0800

    登录消息处理优化，可连接数据库

--- method/serve.py
+++ method/serve.py
@@ -14,22 +14,22 @@ class Service:
         while True:
             data, address = self.sock.recvfrom(4096)
             try:
-                header, date, user, payload = data.decode('utf-8').split("\n\n",3)
-                print(header,date,user,payload)
-                ack_mag = f'ACK\n\n{header}\n\n{date}\n\n{user}'.encode('utf-8')
-                self.sock.sendto(ack_mag, address)
+                header, date, user, payload = data.decode('utf-8').split("\n\n", 3)
+                print(header, date, user, payload)
                 method = {
-                    'LOGIN': [self.login,address,user,payload],
-                    'REGISTER': [self.register,user,payload],
-                    'MESSAGE': [self.message,date,user,payload],
+                    'LOGIN': [self.login, address, user, payload],
+                    'REGISTER': [self.register, user, payload],
+                    'MESSAGE': [self.message, date, user, payload],
                     'UPLOAD': self.upload,
-                    'DOWNLOAD':self.download,
-                    'ONLINE':[self.online,address,user]
+                    'DOWNLOAD': self.download,
+                    'ONLINE': [self.online, address, user]
                 }
-
                 method[header][0](*(method[header][1:]))
-                ack_pak = f'{header}{date}{user}'
-                print(f'ACK to {address}, {header}, {user}, hash:{hash(ack_pak)}')
+                if header not in ['LOGIN',"REGISTER"]:
+                    ack_mag = f'ACK\n\n{header}\n\n{date}\n\n{user}'.encode('utf-8')
+                    self.sock.sendto(ack_mag, address)
+                    ack_pak = f'{header}{date}{user}'
+                    print(f'ACK to {address}, {header}, {user}, hash:{hash(ack_pak)}')
             except Exception as e:
                 print(e)
                 print(data.decode('utf-8'))
@@ -41,10 +41,8 @@ class Service:
             self.ip_pool.append((name,address))
 
     def login(self,address,user,payload):
-        date
-        msg = f"LOGIN_BACK\n\n{date}"
-        self.sock.sendto()
-        return self.SQL_obj.login_check(user,payload)
+        flag = self.SQL_obj.login_check(user,payload)
+        self.sock.sendto(str(flag).encode("utf-8"),address)
         
 
     def register(self,user,payload):
@@ -58,7 +56,6 @@ class Service:
             msg = f'MESSAGE\n\n{date}\n\n{name}\n\n{payload}'.encode('UTF-8')
             self.sock.sendto(msg, address)
 
-        # TODO: SQL操作
         pass
 
     def upload(self):
