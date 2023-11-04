cb04c80e0961a4c9e241aa01352e175d9a9a22bd
mengyonghao <1533512157@qq.com>
2023-11-04 22:41:52 +0800

    修复用户登录bug，删除输入框滚动条，实现消息发送功能

--- page/chat_page.py
+++ page/chat_page.py
@@ -1,10 +1,15 @@
 from tkinter import *
 from tkinter import ttk
+import tkinter.messagebox
 from page import addfriend_page
 
 
 class ChatGui:
-    def __init__(self, init_window_name):
+    def __init__(self, init_window_name, tools):
+        self.tools = tools
+
+        self.chat_model = [0,None] # [is私聊,目标]
+
         self.init_window_name = init_window_name
 
         self.init_window_name.title("chat")  # 窗口名
@@ -17,21 +22,21 @@ class ChatGui:
         s3=Scrollbar(self.init_window_name)
         s3.place(x=775,y=20,height=320)
         self.init_data_Text = Text(self.init_window_name, width=110, height=23)
-
         self.init_data_Text.place(x=0,y=30)
         self.init_data_Text.configure(state='normal') #可以写
         self.init_data_Text.insert(INSERT, 'Hello, World!\n')
         self.init_data_Text.configure(state='disabled') #只读不写
         s3.config(command=self.init_data_Text.yview)
-        s1 = Scrollbar(self.init_window_name)
-        s1.place(x=775, y=470)
-        self.log_data_Text = Text(self.init_window_name, width=110, height=4, yscrollcommand=s1.set)
-        s1.config(command=self.log_data_Text.yview)
-        self.log_data_Text.place(x=0,y=470)
+
+        self.input_Text = Text(self.init_window_name, width=110, height=4)
+        self.input_Text.place(x=0,y=470)
+
         self.send_button = Button(self.init_window_name, text="send", bg="lightblue", width=10,command=self.send)
         self.send_button.place(x=690,y=540)
+
         self.face_button = Button(self.init_window_name, text="表情包", width=10, )
         self.face_button.place(x=440,y=435)
+
         self.file_button = Button(self.init_window_name, text="文件", width=10, )
         self.file_button.place(x=620,y=435)
 
@@ -39,9 +44,6 @@ class ChatGui:
         self.fri_btn = Label(self.init_window_name, text="好友列表")
         self.fri_btn.place(x=850, y=2)
 
-        # 群按钮
-        # self.cla_btn = Button(self.init_window_name, text="群聊",command=lambda :switch(init_window_name))
-        # self.cla_btn.place(x=920, y=2)
 
         # 添加好友
         self.into_fri_btn = Button(self.init_window_name, text="添加好友",command=lambda :self.switch(self.init_window_name))
@@ -69,11 +71,16 @@ class ChatGui:
             print(self.fri_list.selection())
         self.fri_list.bind("<Double-Button-1>", mouse_clicked)
 
+    def message(self):
+        pass
+
     def send(self):
-        self.init_data_Text.configure(state='normal')
-        self.init_data_Text.insert(INSERT, 'Hello, World!\n')
-        self.init_data_Text.configure(state='disabled')
-        self.init_data_Text.update()
+        msg = str(self.chat_model) + '\n' + self.input_Text.get('1.0','end')[:-1]
+        flag = self.tools.chat(msg)
+        if not flag:
+            tkinter.messagebox.showerror('Error', '连接服务器超时请重试')
+        else:
+            self.input_Text.delete('1.0','end')
 
     def switch(self, oldwin):
         init_window = Tk()
