2cd17ba488ac7892d3cc4424d928f8fe571d4d4d
mengyonghao <1533512157@qq.com>
2023-11-20 12:44:26 +0800

    bugfix

--- method/service/serve.py
+++ method/service/serve.py
@@ -10,6 +10,7 @@ logging.basicConfig(filename='log.txt',
                     level=logging.DEBUG)
 
 
+
 class Service:
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
