6bddb2f4887e828c31ab521a3341c2a924598fc5
mengyonghao <1533512157@qq.com>
2023-11-22 13:52:53 +0800

    ack协议确认优化，用户操作登录确认，新增函数鉴权，改为守护线程模式运行

--- method/service/serve.py
+++ method/service/serve.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 import trace
 import time
 import traceback
@@ -16,28 +17,41 @@ class Service:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind(('0.0.0.0', 10088))
         self.ip_pool = {}
+        self.sessions = {}
         self.SQL_obj = SQLTools.SQL_Operate()
+        self.ack_life = []
+
+
+    @staticmethod
+    def logged_in(func):
+        def check(self,*args):
+            if args[0] in self.ip_pool:
+                return func(self,*args)
+        return check
 
     def listen(self):
+        print('开始监听')
         while True:
-            print('开始监听')
             try:
                 data, address = self.sock.recvfrom(4096)
+                print(address)
                 header, date, user, payload = data.decode('utf-8').split("\n\n", 3)
                 if header not in ['LOGIN',"REGISTER"]:
-                    ack_mag = f'ACK\n\n{header}\n\n{date}\n\n{user}'.encode('utf-8')
-                    self.sock.sendto(ack_mag, address)
                     ack_pak = f'{header}{date}{user}'
-                    logging.debug(f'ACK to {address}, {header}{date}{user}, hash:{hash(ack_pak)}')
+                    self.ack(header,date,user,address)
+                    if ack_pak in self.ack_life:
+                        continue
+                    kill = threading.Thread(target=self.kill_ack_message,args=(ack_pak,))
+                    kill.start()
                 method = {
-                    'LOGIN': [self.login, address, user, payload],
-                    'REGISTER': [self.register, address, user, payload],
-                    'MESSAGE': [self.message, date, user, payload],
+                    'LOGIN': [self.login, user, address, payload],
+                    'REGISTER': [self.register, user, address, payload],
+                    'MESSAGE': [self.message, user, date, payload],
                     'GET_MESSAGE_HISTORY': [self.get_msg,user,address],
-                    'UPLOAD': self.upload,
-                    'DOWNLOAD': self.download,
-                    'ONLINE': [self.online, address, user],
-                    'GET_CHATS':[self.get_chats,address,user],
+                    'UPLOAD': [self.upload, user],
+                    'DOWNLOAD': [self.download, user],
+                    'ONLINE': [self.online, user, address],
+                    'GET_CHATS':[self.get_chats,user,address],
                     'LOGOUT':[self.logout, user, address]
                 }
                 method[header][0](*(method[header][1:]))
@@ -49,27 +63,42 @@ class Service:
                 traceback.print_exc()
                 self.sock.sendto('ERROR\n\n \n\n \n\n '.encode('utf-8'),address)
 
-    def online(self,address:tuple ,name:str) -> None:
-        if name in self.ip_pool and self.ip_pool[name] !=  address:
-            self.sock.sendto('LOGOUT\n\n \n\n \n\n'.encode('utf-8'),self.ip_pool[name])
-        self.ip_pool[name]=address
+    def ack(self,header: str,date: str,user: str,address: tuple[str,int]):
+        ack_mag = f'ACK\n\n{header}\n\n{date}\n\n{user}'.encode('utf-8')
+        self.sock.sendto(ack_mag, address)
+        ack_pak = f'{header}{date}{user}'
+        logging.debug(f'ACK to {address}, {ack_pak}, hash:{hash(ack_pak)}')
 
-    def logout(self,user,address):
-        print(user)
-        self.sock.sendto('LOGOUT\n\n \n\n \n\n'.encode('utf-8'), address)
+    def kill_ack_message(self,ack_pak):
+        self.ack_life.append(ack_pak)
+        time.sleep(2)
+        self.ack_life.pop(0)
 
-    def login(self,address,user,payload):
+
+    def online(self,user: str ,address: tuple) -> None:
+        if user in self.ip_pool and self.ip_pool[user] !=  address:
+            self.sock.sendto('LOGOUT\n\n \n\n \n\n'.encode('utf-8'),self.ip_pool[user])
+        self.ip_pool[user]=address
+
+    def login(self,user: str ,address: tuple ,payload: str) -> None:
         flag = self.SQL_obj.login_check(user,payload)
+        if flag == 1:
+            self.ip_pool[user] = address
         self.sock.sendto(str(flag).encode("utf-8"),address)
         logging.info(f"address:{address} user:{user} res:{flag}")
-        
 
-    def register(self,address,user,payload):
+    def register(self,user: str,address: tuple,payload: str) -> None:
         flag = self.SQL_obj.register(user,payload)
         self.sock.sendto(str(flag).encode("utf-8"), address)
         logging.info(f"address:{address} user:{user} res:{flag}")
 
-    def message(self, date: str, user, payload):
+    @logged_in
+    def logout(self,user: str ,address: tuple) -> None:
+        del self.ip_pool[user]
+        self.sock.sendto('LOGOUT\n\n \n\n \n\n'.encode('utf-8'), address)
+
+    @logged_in
+    def message(self, user: str, date: float, payload: str) -> None:
         target, msg = payload.split('\n', 1)
         target = json.loads(target)
         model = target[0] # is私聊
@@ -84,7 +113,8 @@ class Service:
                     self.sock.sendto(f'MESSAGE\n\n{date}\n\n{user}\n\n{payload}'.encode('utf-8'), self.ip_pool[member])
         self.SQL_obj.save_msg(date,user,model,target[1],msg)
 
-    def get_msg(self,user,address):
+    @logged_in
+    def get_msg(self,user: str,address: tuple) -> None:
         msgs = self.SQL_obj.get_msg(1,user)
         for msg in msgs:
             msg = list(msg[:3]) + [msg[3].strftime('%Y-%m-%d %H:%M:%S')] + list(msg[4:])
@@ -96,18 +126,27 @@ class Service:
 
         self.sock.sendto('HISTORY\n\n\n\n2\n\n'.encode('utf-8'),address)
 
-    def get_chats(self,address,user):
+    @logged_in
+    def get_chats(self,user: str,address: tuple) -> None:
         chat_list = self.SQL_obj.get_chat_list(user)
         payload = f'CHAT_LIST\n\n \n\n \n\n{json.dumps(chat_list)}'
         self.sock.sendto(payload.encode('utf-8'),address)
 
-    def upload(self):
+    @logged_in
+    def upload(self,user):
         pass
 
-    def download(self):
+    @logged_in
+    def download(self,user):
         pass
 
 if __name__ == "__main__":
     service = Service()
-    # service.SQL_obj.get_msg(0,'admin')
-    service.listen()
+    listen = threading.Thread(target=service.listen)
+    listen.daemon=True
+    listen.start()
+    while True:
+        try:
+            time.sleep(10)
+        except KeyboardInterrupt:
+            print('service stopped')
