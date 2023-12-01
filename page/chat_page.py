a9da2e7b06c2a236dfe210f5418a71dab8d2d237
mengyonghao <1533512157@qq.com>
2023-12-01 23:01:38 +0800

    修改requests子列表iid

--- page/chat_page.py
+++ page/chat_page.py
@@ -17,7 +17,6 @@ class ChatGui(tk.Tk):
         self.btn_addfri = tk.Button
         self.chat_list = ttk.Treeview()
         self.chat_page = tk.StringVar()
-        self.chat_page.set('system')
         self.fri_list = ''
         self.group_list = ''
         self.run()
@@ -63,8 +62,8 @@ class ChatGui(tk.Tk):
         s2.config(command=self.chat_list.yview)
         # 好友分组
         self.chat_list.insert('', index='end', tags=[json.dumps([1, 'system'])], text='system')
-        self.fri_list = self.chat_list.insert('', 1, 'second', text='好友', )
-        self.group_list = self.chat_list.insert('', 2, 'third', text='群聊', )
+        self.fri_list = self.chat_list.insert('', 1, 'friends', text='好友', )
+        self.group_list = self.chat_list.insert('', 2, 'groups', text='群聊', )
 
     def get_msg(self,date: str,user: str,message: str):
         self.msg.configure(state='normal')
