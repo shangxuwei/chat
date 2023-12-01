93efb9402f0864ad8d87b0e012cad509c02e92d8
mengyonghao <1533512157@qq.com>
2023-12-01 23:00:51 +0800

    添加好友请求发送成功消息弹窗 修改搜索结果背景色

--- page/addfriend_page.py
+++ page/addfriend_page.py
@@ -1,5 +1,6 @@
 import json
 import tkinter as tk
+import tkinter.messagebox
 from tkinter import ttk
 
 class AddGui(tk.Tk):
@@ -7,9 +8,9 @@ class AddGui(tk.Tk):
         super().__init__()
         self.title("添加好友")  # 窗口名
         self.geometry('350x540+10+10')
+        self.resizable(width=False, height=False)
         self.search_Text = tk.Text()
         self.search_Button = tk.Button()
-        self.add_Button = tk.Button()
         self.friend = tk.Label()
         self.group = tk.Label()
         self.addfri_Button = tk.Button()
@@ -24,7 +25,7 @@ class AddGui(tk.Tk):
     def run(self):
         tk.Label(self,text='好友用户名').place(x=5,y=5)
 
-        self.search_Text = tk.Text(self)
+        self.search_Text = tk.Entry(self)
         self.search_Text.place(x=80,y=0,height=30,width=200)
 
         self.search_Button = tk.Button(self,text="搜索")
@@ -32,14 +33,14 @@ class AddGui(tk.Tk):
 
         tk.Canvas(self,width=350,height=50,bg='white').place(x=0,y=60)
 
-        self.friend = tk.Label(self,text="")
+        self.friend = tk.Label(self,bg='white')
         self.friend.place(x=50,y=62)
         ttk.Separator(self, orient='horizontal').place(x=0,y=87,width=350)
-        self.group = tk.Label(self,text="")
+        self.group = tk.Label(self,bg='white')
         self.group.place(x=50,y=88)
 
-        self.addfri_Button = tk.Button(self,text='添加好友')
-        self.addgroup_Button = tk.Button(self,text='添加群聊')
+        self.addfri_Button = tk.Button(self,text='添加好友',state='disabled')
+        self.addgroup_Button = tk.Button(self,text='添加群聊',state='disabled')
         self.addfri_Button.place(x=280,y=64,height=20)
         self.addgroup_Button.place(x=280,y=90,height=20)
 
@@ -59,6 +60,10 @@ class AddGui(tk.Tk):
 
         self.request_list.item('friends',open=True)
         self.request_list.item('groups',open=True)
+
+    @staticmethod
+    def sent_request(title):
+        tkinter.messagebox.showinfo(title,'发送请求成功')
 
 if __name__ == '__main__':
     page = AddGui()
